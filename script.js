1949c2b2d6f857a32ffe9d4b3c719b0cba8544ad
Nagy-Bana Éva <nagybanaeva@gmail.com>
2020-06-17 21:43:44 +0200

    add files

---
+++ script.js
@@ -0,0 +1,46 @@
+var Fradi = {
+  'goalkeepers': {
+	  1: 'Gergo Szecsi',
+	  90: 'Denes Dibusz',
+	  99: 'David Grof',
+  } ,
+	
+  'defenders': {
+	  5: 'Abraham Frimpong',
+	  8: 'Gergo Lovrencsics',
+	  16: 'Leandro De Almeida',
+	  17: 'Eldar Civic',
+	  21: 'Endre Botka',
+	  22: 'Otigba Kenneth',
+	  25: 'Miha Blazic',
+	  26: 'Marcel Heister',
+	  33: 'Lasha Dvali',
+	  53: 'Dominik Csontos',
+  },
+	
+  'midfielders': {
+	  7: 'Wergiton Somalia',
+	  14: 'Ihor Haratin',
+	  18: 'David Siger',
+	  19: 'Balint Vecsei',
+	  27: 'Gaston Lodico',
+	  51: 'Andras Csonka',
+	  69: 'Rego Szantho',
+	  92: 'Michal Skvarka',
+  },
+	
+  'forwards': {
+	  10: 'Tokmac Chol Nguen',
+	  11: 'Oleksandor Zubkov',
+	  20: 'Mikalai Sihnevich',
+	  70: 'Frank Boli',
+	  77: 'Krisztofer Szereto',
+	  88: 'Isael Da Silva Barbosa',
+	  97: 'Roland Varga',
+  }
+};
+
+document.write (Fradi);
+
+document.write (Fradi.goalkeepers['1']);
+
